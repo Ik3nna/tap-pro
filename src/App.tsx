1fc32e03caa8dee931943887e59eaa85fe60df06
ik3nna <iamiykendukwu@gmail.com>
2025-07-10 17:19:18 +0100

    feat: Final mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -52,12 +52,15 @@ function App() {
 
     if (showModal) {
       document.addEventListener("mousedown", handleClickOutside);
+      document.body.style.overflow = 'hidden';
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.body.style.overflow = '';
     }
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.body.style.overflow = '';
     };
   }, [showModal]);
   
@@ -265,125 +268,42 @@ function App() {
           )}
         </AnimatePresence>
       </section>
-      
-      
-      {/* <section className="header" ref={modalRef}>
-        <div className="flex">
-          <img className="tapro" alt="Tapro icon" src="https://d1wqzb5bdbcre6.cloudfront.net/182832760f652d8d4cb33cb14428cd4acbabc67629b9192c99d551950baead7d/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a64463878555756344e5842484e6a5656567a5a3464566f3366475a6662476c325a56396951546c6e4e486c3259556f34537a4a42646b784553586f77566e51334d455130304975716c5a50654a/6d65726368616e745f69643d616363745f31516578357047363555573678755a3726636c69656e743d5041594d454e545f50414745" />
-          <div>Tapro</div>
-        </div>
 
-        <div className="flex" onClick={() => setShowModal(!showModal)} >
-          <div>{showModal ? "Close" : "Details"}</div>
-          <IoIosArrowDown className={`arrow ${showModal ? "open" : ""}`} />
-        </div>
-
-        <AnimatePresence>
-          {showModal && (
-            <motion.div
-              className="dropdown-wrapper"
-              initial={{ opacity: 0, y: -20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              transition={{ duration: 0.3 }}
-            >
-              <div className="dropdown-menu">Dropdown Content</div>
-              <div className="partial-overlay" onClick={() => setShowModal(false)} />
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </section>
-
-      <section className="content">
-        <motion.img 
-          className="tapro" 
-          alt="Tapro icon" 
-          {...defaultMotionProps} 
-          variants={fadeUpVariant} 
-          transition={createTransition(1, 0.3)}
-          src="https://d1wqzb5bdbcre6.cloudfront.net/182832760f652d8d4cb33cb14428cd4acbabc67629b9192c99d551950baead7d/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a64463878555756344e5842484e6a5656567a5a3464566f3366475a6662476c325a56396951546c6e4e486c3259556f34537a4a42646b784553586f77566e51334d455130304975716c5a50654a/6d65726368616e745f69643d616363745f31516578357047363555573678755a3726636c69656e743d5041594d454e545f50414745" 
-        />
-
-        <div className="description">
-          <motion.p
-            {...defaultMotionProps} 
-            variants={fadeUpVariant} 
-            transition={createTransition(1, 0.2)}
-          >
-            Try BETA Business Tier
-          </motion.p>
-          <motion.p
-            {...defaultMotionProps} 
-            variants={fadeUpVariant} 
-            transition={createTransition(1, 0.3)}
-          >
-            €350.00
-          </motion.p>
-          <motion.p
-            {...defaultMotionProps} 
-            variants={fadeUpVariant} 
-            transition={createTransition(1, 0.4)}
-          >
-            Then €100.00 per year
-          </motion.p>
-        </div>
-
-        <AnimatePresence>
-          {showPayButton && (
-            <motion.div
-              className="pay"
-              variants={fadeUpVariant}
-              initial="hidden"
-              animate="visible"
-              exit="hidden"
-              transition={{ duration: 0.8 }}
-            >
-              Pay
-            </motion.div>
-          )}
-        </AnimatePresence>
-
-        <motion.article layout>
-          <div className="line">
-            <motion.hr 
-              {...defaultMotionProps} 
-              variants={fadeLeftVariant} 
-              transition={createTransition(1, 0.5)}
-            />
-             
-            <motion.div
-              {...defaultMotionProps} 
-              variants={fadeDownVariant} 
-              transition={createTransition(1, 0.5)}
-            >
-              or
-            </motion.div> 
-            
-            <motion.hr 
-              {...defaultMotionProps} 
-              variants={fadeRightVariant} 
-              transition={createTransition(1, 0.5)}
-            />
+      {isMobile && (
+        <div ref={modalRef} className="mobile-header">
+          <div className="flex">
+            <img className="tapro" alt="Tapro icon" src="https://d1wqzb5bdbcre6.cloudfront.net/182832760f652d8d4cb33cb14428cd4acbabc67629b9192c99d551950baead7d/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a64463878555756344e5842484e6a5656567a5a3464566f3366475a6662476c325a56396951546c6e4e486c3259556f34537a4a42646b784553586f77566e51334d455130304975716c5a50654a/6d65726368616e745f69643d616363745f31516578357047363555573678755a3726636c69656e743d5041594d454e545f50414745" />
+            <div>Tapro</div>
           </div>
 
-          <div>
-            <motion.p
-              {...defaultMotionProps} 
-              variants={fadeUpVariant} 
-              transition={createTransition(1, 0.4)}
-            >
-              Email
-            </motion.p>
-            <motion.input 
-              type="email" 
-              placeholder="Enter your email" 
-              {...defaultMotionProps} 
-              variants={fadeUpVariant} 
-              transition={createTransition(1, 0.5)}
-            />
+          <div className="flex" onClick={() => setShowModal(!showModal)} >
+            <div>{showModal ? "Close" : "Details"}</div>
+            <IoIosArrowDown className={`arrow ${showModal ? "open" : ""}`} />
           </div>
-        </motion.article>
-      </section> */}
+
+          <AnimatePresence>
+            {showModal && (
+             <motion.div
+                className="dropdown-wrapper"
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: 300, opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+              >
+                <div className="dropdown-menu">Dropdown Content</div>
+                <motion.div
+                  className="partial-overlay"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  onClick={() => setShowModal(false)}
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      )}
     </main>
   )
 }
@@ -391,12 +311,6 @@ function App() {
 export default App
 
 
-
-const defaultMotionProps = {
-  initial: "hidden",
-  whileInView: "visible",
-  viewport: { once: true, amount: 0.3 },
-};
 const fadeUpVariant = {
   hidden: { opacity: 0, y: 30 },
   visible: { opacity: 1, y: 0 }
@@ -406,8 +320,3 @@ const snapshotVariant = {
   animate: { opacity: 1, scale: 1, transition: { duration: 0.2, ease: easeInOut } },
   exit: { opacity: 0, scale: 0.98, transition: { duration: 0.3, ease: easeInOut } },
 };
-const createTransition = (duration: number, delay: number) => ({
-  duration,
-  delay,
-  ease: easeInOut
-});
