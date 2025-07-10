cb0f3bd2009f7b90ff448a12d3ae4bb283ab9125
ik3nna <iamiykendukwu@gmail.com>
2025-07-10 16:31:16 +0100

    feat: Mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,39 @@ import Skeleton from "./component/skeleton";
 function App() {
   const [loading, setLoading] = useState(false)
   const [showModal, setShowModal] = useState(false);
-  const modalRef = useRef<HTMLDivElement | null>(null);
   const [showPayButton, setShowPayButton] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
+  const modalRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     setLoading(true);
+  
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1500); 
+    }, 1500);
+  
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+  
+    handleResize();
+    window.addEventListener("resize", handleResize);
 
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
-
+  
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShowPayButton(true);
-    }, 3000);
-
-    return () => clearTimeout(timeout);
-  }, []);
+    if (isMobile && !showPayButton) {
+      const timeout = setTimeout(() => {
+        setShowPayButton(true);
+      }, 3000);
+  
+      return () => clearTimeout(timeout);
+    }
+  }, [isMobile, showPayButton]);
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
@@ -167,12 +181,23 @@ function App() {
               animate="animate"
               exit="exit"
             >
-              <Skeleton className="payment-deet" variant="rect" style={{ width: "150px", height: "28px" }} />
+              {isMobile ? (
+                <div className="mobile-payment-deet">
+                  <Skeleton className="mobile-tapro" variant="rect" />
+
+                  <div className="description">
+                    <Skeleton variant="rect" style={{ width: "200px", height: "20px" }} />
+                    <Skeleton variant="rect" style={{ width: "100px", height: "20px", margin: "auto" }} />
+                    <Skeleton variant="rect" style={{ width: "200px", height: "20px" }} />
+                  </div>
+                </div>
+                ) : (
+                <Skeleton className="payment-deet" variant="rect" style={{ width: "150px", height: "28px" }} />
+              )}
              
               <div className="container2-flex">
                 <Skeleton className="payment-deet" variant="rect" style={{ width: "100px", height: "28px" }} />
-
-                <Skeleton className="payment-deet" variant="rect" style={{ width: "400px", height: "30px" }} />
+                <Skeleton className="payment-deet" variant="rect" style={{ width: isMobile ? "100%" : "400px", height: "30px" }} />
               </div>
 
               <div className="container2-flex">
@@ -187,20 +212,55 @@ function App() {
               animate="animate"
               exit="exit"
             >
-              <div className="payment-deet">Enter payment details</div>
+              {isMobile ? (
+                <div className="mobile-payment-deet">
+                  <img 
+                    className="mobile-tapro" 
+                    alt="Tapro icon" 
+                    src="https://d1wqzb5bdbcre6.cloudfront.net/182832760f652d8d4cb33cb14428cd4acbabc67629b9192c99d551950baead7d/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a64463878555756344e5842484e6a5656567a5a3464566f3366475a6662476c325a56396951546c6e4e486c3259556f34537a4a42646b784553586f77566e51334d455130304975716c5a50654a/6d65726368616e745f69643d616363745f31516578357047363555573678755a3726636c69656e743d5041594d454e545f50414745" 
+                  />
 
-              <div className="container2-flex">
-                <p>Email</p>
-                <input 
-                  type="email" 
-                  placeholder="Enter your email" 
-                />
-              </div>
+                  <div className="description">
+                    <p>Try BETA Business Tier</p>
+                    <p>€350.00</p>
+                    <p>Then €100.00 per year</p>
+                  </div>
+                </div>
+              ) : (
+                <div className="payment-deet">Enter payment details</div>
+              )}
 
-              <div className="container2-flex">
-                <div className="payment-meth">Payment method</div>
-                <div className="method" />
-              </div>
+              {isMobile && (
+                <AnimatePresence>
+                  {showPayButton && (
+                    <motion.div
+                      className="pay-button"
+                      variants={fadeUpVariant}
+                      initial="hidden"
+                      animate="visible"
+                      exit="hidden"
+                      transition={{ duration: 0.8 }}
+                    >
+                      Pay
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              )}
+
+              <motion.article layout>
+                <div className="container2-flex">
+                  <p>Email</p>
+                  <input 
+                    type="email" 
+                    placeholder="Enter your email" 
+                  />
+                </div>
+
+                <div className="container2-flex">
+                  <div className="payment-meth">Payment method</div>
+                  <div className="method" />
+                </div>
+              </motion.article>
             </motion.article>
           )}
         </AnimatePresence>
@@ -336,6 +396,10 @@ const defaultMotionProps = {
   initial: "hidden",
   whileInView: "visible",
   viewport: { once: true, amount: 0.3 },
+};
+const fadeUpVariant = {
+  hidden: { opacity: 0, y: 30 },
+  visible: { opacity: 1, y: 0 }
 };
 const snapshotVariant = {
   initial: { opacity: 0, scale: 0.98 },
